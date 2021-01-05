63210e7527671470385ecb35ae8962c865ff2813
LukaszProkop <wuzu222@gmail.com>
2021-01-06 00:42:19 +0100

    Add I-shaped block
    This commit implements IBlock and adds it to the list in the BlockFeed class.

---
+++ src/main/java/com/epam/prejap/tetris/block/IBlock.java
@@ -0,0 +1,25 @@
+package com.epam.prejap.tetris.block;
+
+/**
+ * I-shaped block implementation of the {@link Block} abstraction class.
+ * <br><br>
+ * <div style="text-indent: 20;"> Implementation note: <p>This class
+ * implements static 2d array in order to create required "I" shape block
+ * </p></div>
+ * <br>
+ * @see Block
+ * @author Łukasz Prokop
+ */
+final class IBlock extends Block {
+
+    private static final byte[][] IMAGE = {
+            {1},
+            {1},
+            {1},
+            {1},
+    };
+
+    IBlock() {
+        super(IMAGE);
+    }
+}
