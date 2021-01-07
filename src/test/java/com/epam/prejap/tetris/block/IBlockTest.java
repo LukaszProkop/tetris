48a60850a6f72dd1cac5faeb27d7e7ababdf4e1f
LukaszProkop <wuzu222@gmail.com>
2021-01-08 00:22:54 +0100

    Refactor IBlockTest and pom.xml
    This commit removes the Reflection API responsible for finding the private block image inside the IBlock class, Block.image will be used instead of reflection.
    The Tetris version also has been updated from 0.1 to 0.3 for LBlock and IBlock

--- src/test/java/com/epam/prejap/tetris/block/IBlockTest.java
+++ src/test/java/com/epam/prejap/tetris/block/IBlockTest.java
@@ -3,80 +3,61 @@ package com.epam.prejap.tetris.block;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotEquals;
 
-@Test(groups = "IBlock")
+@Test(groups = "Block")
 public class IBlockTest {
 
     //given
     @Test(dataProvider = "iBlockShape")
-    public void testIBlockShape(byte[][] expected) throws IllegalAccessException {
+    public void testIBlockShape(byte[][] expected) {
         //when
-        byte[][] actual = getIBlock();
+        byte[][] actual = new IBlock().image;
 
         //then
         assertEquals(actual, expected);
-    }
-
-    //given
-    @Test(dataProvider = "shapeOfBlocks")
-    public void testIBlockShapeAgainstOtherShapes(byte[][] anyBlock) throws IllegalAccessException {
-        //when
-        byte[][] actual = getIBlock();
-
-        //then
-        assertNotEquals(anyBlock, actual);
     }
 
     //given
     @Test(dataProvider = "iBlockShape")
-    public void testIfBlockConstructorAcceptIBlock(byte[][] expected) throws IllegalAccessException {
-        //given
-        byte[][] iBlock = getIBlock();
-
+    public void testIfBlockConstructorAcceptIBlock(byte[][] expected) {
         //when
-        Block block = new Block(iBlock) {
-        };
-        byte[][] actual = block.image;
+        byte[][] actual = new IBlock().image;
 
         //then
         assertEquals(actual, expected);
-
     }
 
-    private static byte[][] getIBlock() throws IllegalAccessException {
-        Field[] fields = IBlock.class.getDeclaredFields();
-        Arrays.stream(fields).forEach(field -> field.setAccessible(true));
-        return (byte[][]) Arrays.stream(fields)
-                .filter(field -> field.toString().contains("IMAGE"))
-                .findFirst()
-                .orElse(fields[0])
-                .get(new IBlock());
+    @Test(dataProvider = "dotsForIBlock")
+    public void shallCreateIBlockWithIShapedDots(int row, int col) {
+        //given
+        Block iBlock = new IBlock();
+        int dotMark = 1;
+
+        //when
+        byte actualDot = iBlock.dotAt(row, col);
+
+        //then
+        assertEquals(actualDot, dotMark, "Shall create I block with correct shaped dots, but did not");
     }
 
-    @DataProvider
-    public static Object[][] shapeOfBlocks() {
-        byte[][] lBlock = {{1, 0}, {1, 0}, {1, 1}};
-        byte[][] jBlock = {{0, 1}, {0, 1}, {1, 1}};
-        byte[][] sBlock = {{0, 1, 1}, {1, 1, 0}};
-        byte[][] zBlock = {{1, 1, 0}, {0, 1, 1}};
-        byte[][] oBlock = {{1, 1}, {1, 1}};
+    @DataProvider()
+    public static Object[] dotsForIBlock() {
         return new Object[][]{
-                {lBlock},
-                {jBlock},
-                {sBlock},
-                {zBlock},
-                {oBlock}
+                {0, 0},
+                {1, 0},
+                {2, 0},
+                {3, 0},
         };
     }
 
     @DataProvider
     public static Object[] iBlockShape() {
-        byte[][] iBlock = {{1}, {1}, {1}, {1}};
+        byte[][] iBlock = {
+                {1},
+                {1},
+                {1},
+                {1}};
         return new Object[]{iBlock};
     }
 }
